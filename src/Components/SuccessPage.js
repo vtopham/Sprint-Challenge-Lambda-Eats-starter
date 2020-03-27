8ba46a5642bdd4f03b2a7e28adb36aa17ad30f47
Victoria  Topham <topham.victoria@gmail.com>
2020-03-27 09:59:38 -0700

    successfully added a success page

--- src/Components/SuccessPage.js
+++ src/Components/SuccessPage.js
@@ -1,11 +1,25 @@
 import React from "react"
 
 
-function SuccessPage(prope) {
+function SuccessPage(props) {
+    const {postRes, setPostRes} = props
 
     return(
         <>
-        <p>This is the success page</p>
+       
+        <div>
+            <h1 data-cy = "success">Congrats! Pizza is on its way.</h1>
+            <p>Name: {postRes.name}</p>
+            <p>Size: {postRes.size}</p>
+            <p>Toppings:</p>
+                <ul>
+                    {postRes.extraCheese ? <li>Extra Cheese</li> : null }
+                    {postRes.birthdayCake ? <li>A whole birthday cake</li> : null }
+                    {postRes.sawdust ? <li>Sawdust</li> : null }
+                    {postRes.gummyWorms ? <li>Gummyworms</li> : null }
+                </ul> 
+            <p>Special Instructions: {postRes.instructions}</p>
+        </div>
         </>
     )
 }
