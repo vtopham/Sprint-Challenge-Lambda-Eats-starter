798cebf8f9589f1c1f441bbf013a37af44a926f8
Victoria  Topham <topham.victoria@gmail.com>
2020-03-27 08:52:59 -0700

    form state is working

--- src/Components/PizzaForm.js
+++ src/Components/PizzaForm.js
@@ -1,18 +1,42 @@
-import React from "react"
+import React, {useState} from "react"
 import Header from "./Header"
 
+
+
+
+
+
+
 function PizzaForm(props) {
+    const [formInput, setFormInput] = useState({
+        name: "",
+        size: "",
+        extraCheese: false,
+        gummyWorms: false,
+        birthdayCake: false,
+        sawdust: false,
+        instructions: ""
+    })
+
+    const inputChange = (event) => {
+        setFormInput({
+            ...formInput,
+            [event.target.name]: event.target.type === "checkbox" ? event.target.checked : event.target.value
+        })
+
+    }
+
     return (
         <>
         <Header/>
         <h1> Let's order a pizza.</h1>
         <form>
             <label htmlFor = "name">Name: </label>
-            <input type = "text" name = "name" id = "name"/>
+            <input onChange = {inputChange} type = "text" name = "name" id = "name" value = {formInput.name}/>
             <br/>
 
             <label htmlFor = "size">Size: </label>
-            <select name = "size" id = "size">
+            <select onChange = {inputChange} name = "size" id = "size" value = {formInput.size}>
                 <option value = "10 inch">10"</option>
                 <option value = "150 inch">150"</option>
             </select>
@@ -20,21 +44,21 @@ function PizzaForm(props) {
 
             <p>Toppings</p>
             <div class = "toppings">
-                <input type = "checkbox" name = "extraCheese"id = "extraCheese"/>
+                <input onChange = {inputChange} type = "checkbox" name = "extraCheese"id = "extraCheese" checked = {formInput.extraCheese}/>
                 <label htmlFor = "extraCheese">Extra cheese</label>
 
-                <input type = "checkbox" name = "gummyWorms"id = "gummyWorms"/>
+                <input onChange = {inputChange} type = "checkbox" name = "gummyWorms"id = "gummyWorms" checked = {formInput.gummyWorms}/>
                 <label htmlFor = "gummyWorms">Gummy worms</label>
 
-                <input type = "checkbox" name = "birthdayCake"id = "birthdayCake"/>
+                <input onChange = {inputChange} type = "checkbox" name = "birthdayCake"id = "birthdayCake" checked = {formInput.birthdayCake}/>
                 <label htmlFor = "birthdayCake">A whole birthday cake</label>
 
-                <input type = "checkbox" name = "sawdust"id = "sawdust"/>
+                <input onChange = {inputChange} type = "checkbox" name = "sawdust"id = "sawdust" checked = {formInput.sawdust}/>
                 <label htmlFor = "sawdust">Sawdust</label>
             </div>
 
             <label htmlFor = "instructions">Special Instructions</label> <br/>
-            <textarea name = "instructions" id = "instructions" rows = "4" cols = "50"/>
+            <textarea onChange = {inputChange} name = "instructions" id = "instructions" rows = "4" cols = "50" value = {formInput.instructions}/>
         </form>
         </>
     )
