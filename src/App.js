0440dc79c8c77ebafa77f47320f958e6eb128178
Victoria  Topham <topham.victoria@gmail.com>
2020-03-27 08:17:44 -0700

    got router working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import {Route} from "react-router-dom"
+
+import HomePage from "./Components/HomePage"
+import PizzaForm from "./Components/PizzaForm"
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+    <Route exact path = "/">
+      <HomePage/>
+    </Route>
+    <Route path = "/pizza">
+      <PizzaForm/>
+    </Route>
     </>
   );
 };
---
+++ src/Components/PizzaForm.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function PizzaForm(props) {
+    return (
+        <>
+        <h1> this is a pizza form, bitch</h1>
+        </>
+    )
+}
+
+export default PizzaForm
